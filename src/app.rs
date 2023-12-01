520e40a28b543a2340ab630952036c290a24d81c
JonLiuFYI <2997583+JonLiuFYI@users.noreply.github.com>
2023-12-01 13:31:39 -0500

    Change font and text size, remove unneeded deps

--- src/app.rs
+++ src/app.rs
@@ -4,7 +4,9 @@ mod page_start;
 mod page_timer;
 
 use app_state::{NewspupPage, Round};
-use egui::{CentralPanel, Layout, TopBottomPanel};
+use egui::{
+    CentralPanel, FontData, FontDefinitions, FontFamily, FontId, Layout, TextStyle, TopBottomPanel,
+};
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
@@ -27,6 +29,48 @@ impl NewspupApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+        let mut fonts = FontDefinitions::default();
+        fonts.font_data.insert(
+            "inter".to_owned(),
+            FontData::from_static(include_bytes!("../assets/fonts/Inter-Regular.ttf")),
+        );
+        fonts
+            .families
+            .get_mut(&FontFamily::Proportional)
+            .unwrap()
+            .insert(0, "inter".to_owned());
+        cc.egui_ctx.set_fonts(fonts);
+
+        let mut style = (*cc.egui_ctx.style()).clone();
+        style.text_styles = [
+            (
+                TextStyle::Heading,
+                FontId::new(25.0, FontFamily::Proportional),
+            ),
+            (
+                TextStyle::Name("Heading2".into()),
+                FontId::new(20.0, FontFamily::Proportional),
+            ),
+            (
+                TextStyle::Name("Context".into()),
+                FontId::new(20.0, FontFamily::Proportional),
+            ),
+            (TextStyle::Body, FontId::new(16.0, FontFamily::Proportional)),
+            (
+                TextStyle::Monospace,
+                FontId::new(14.0, FontFamily::Proportional),
+            ),
+            (
+                TextStyle::Button,
+                FontId::new(16.0, FontFamily::Proportional),
+            ),
+            (
+                TextStyle::Small,
+                FontId::new(10.0, FontFamily::Proportional),
+            ),
+        ]
+        .into();
+        cc.egui_ctx.set_style(style);
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
@@ -64,7 +108,6 @@ impl eframe::App for NewspupApp {
                     }
 
                     egui::warn_if_debug_build(ui);
-
 
                     ui.with_layout(Layout::right_to_left(egui::Align::Max), |ui| {
                         if ui.button("New").clicked() {
