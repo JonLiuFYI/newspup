b55920e3530042bd2bd98effba2504c6309e0875
JonLiuFYI <2997583+JonLiuFYI@users.noreply.github.com>
2024-04-06 14:09:12 -0400

    Begin organizing timer UI into screens

--- src/app/page_timer.rs
+++ src/app/page_timer.rs
@@ -15,6 +15,28 @@ impl NewspupApp {
 
         // timer display
         match self.timer_state {
+            // timer setup screen
+            TimerState::Stopped => {
+                ui.heading(format!("{:.1}", self.selected_duration));
+
+                if ui.button("3:00 — Frantic").clicked() {
+                    self.selected_duration = 5.; // 180
+                }
+                if ui.button("4:00 — Standard").clicked() {
+                    self.selected_duration = 10.; // 240
+                }
+                if ui.button("5:00 — Relaxed").clicked() {
+                    self.selected_duration = 15.; // 300
+                }
+
+                if ui.button("Start").clicked() {
+                    self.timer_state = TimerState::Started {
+                        start_time: app_time,
+                        duration: self.selected_duration,
+                    };
+                    dbg!(self.timer_state);
+                }
+            }
             TimerState::Started {
                 start_time,
                 duration,
@@ -32,33 +54,22 @@ impl NewspupApp {
                 if seconds_remaining <= 0. {
                     self.timer_state = TimerState::TimeUp;
                 }
-            }
-            TimerState::Stopped => {
-                if ui.button("3:00 — Frantic").clicked() {
-                    self.selected_duration = 5.; // 180
-                }
-                if ui.button("4:00 — Standard").clicked() {
-                    self.selected_duration = 10.; // 240
-                }
-                if ui.button("5:00 — Relaxed").clicked() {
-                    self.selected_duration = 15.; // 300
-                }
-
-                ui.heading(format!("{:.1}", self.selected_duration));
+                self.timer_controls(ui, seconds_remaining, app_time);
             }
             TimerState::TimeUp => {
                 ui.heading("Time's up!");
+                self.timer_controls(ui, seconds_remaining, app_time);
             }
             TimerState::Paused(seconds_remaining) => {
                 ui.heading(format!("{seconds_remaining:.1} (Paused)"));
+                self.timer_controls(ui, seconds_remaining, app_time);
             }
         }
 
-        // controls
-        let start_btn = ui.add_enabled(
-            self.timer_state == TimerState::Stopped,
-            Button::new("Start"),
-        );
+        ui.ctx().request_repaint();
+    }
+
+    fn timer_controls(&mut self, ui: &mut egui::Ui, seconds_remaining: f64, app_time: f64) {
         let pause_btn = ui.add_enabled(
             matches!(self.timer_state, TimerState::Started { .. }),
             Button::new("Pause"),
@@ -73,13 +84,6 @@ impl NewspupApp {
             Button::new("Reset"),
         );
 
-        if start_btn.clicked() {
-            self.timer_state = TimerState::Started {
-                start_time: app_time,
-                duration: self.selected_duration,
-            };
-            dbg!(self.timer_state);
-        }
         if pause_btn.clicked() {
             self.timer_state = TimerState::Paused(seconds_remaining);
         }
@@ -96,7 +100,5 @@ impl NewspupApp {
         }
 
         ui.label(format!("(Elapsed: {app_time:.1})"));
-
-        ui.ctx().request_repaint();
     }
 }
